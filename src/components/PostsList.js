4dd2b7da0dbcaebf84dd591fa47001efc52a8263
Madhav Tengetol <tengetolmadhav@gmail.com>
2024-01-25 15:53:08 +0530

    useState hook used

---
+++ src/components/PostsList.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
+import Post from "./Post";
+import classes from './PostsList.module.css';
+import Modal from "./Modal";
+function PostsList(){
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+    function bodyChangeHandler(event){
+    setEnteredBody(event.target.value);
+    }
+
+    
+    function authorChangeHandler(event){
+        setEnteredAuthor(event.target.value);
+        }
+return <>
+<Modal>
+<NewPost onBodyChange={bodyChangeHandler} onAuthorChange = {authorChangeHandler}/>
+</Modal>
+    
+    <ul className={classes.posts}>
+    <Post author={enteredAuthor} body={enteredBody}/>
+     <Post author="Tony" body="I am Iron Man"/>
+    </ul>
+    </>
+}
+
+export default PostsList;
